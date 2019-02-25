ace4a55ea3bc2a2050aca9cc3b4877a6324b6326
freepink <45089277+freepink@users.noreply.github.com>
2019-02-25 15:05:47 -0800

    Update Feb-25-Reverse-Integer.py

--- Feb-25-Reverse-Integer.py
+++ Feb-25-Reverse-Integer.py
@@ -6,3 +6,7 @@ class Solution:
             return result if result <= pow(2,31)-1 else 0
         if x < 0:
             return -result if -result >= -pow(2,31) else 0
+
+      '''Runtime: 56 ms, faster than 60.66% of Python3 online submissions for Reverse Integer.
+Memory Usage: 13.3 MB, less than 5.71% of Python3 online submissions for Reverse Integer.'''
+
